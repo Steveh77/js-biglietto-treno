03ee1c11b36dbab7b44a9bdd0eb7a1f38c22e50f
Steveh77 <stederosa76@gmail.com>
2022-06-21 18:04:15 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -13,23 +13,33 @@ console.log(età)
 // km da percorrere * 0.21€ al kilometro
 
 const prezzoPerChilometro = kmPercorsi * (0.21)
-console.log(prezzoPerChilometro)
-
+console.log(prezzoPerChilometro.toFixed(2) + "€")
+document.getElementById("a").innerHTML= "Il prezzo in base ai km è: " + prezzoPerChilometro.toFixed(2) + "€"
 
 // sconto in base all'età
 
 let sconto ; 
+let prezzoFinale ;
 
 if (età < 18){
     sconto = prezzoPerChilometro * 20 / 100
+    document.getElementById("b").innerHTML= "Lo sconto è: " + sconto.toFixed(2) + "€"
+    console.log(sconto)
+    prezzoFinale = prezzoPerChilometro - sconto;
+    document.getElementById("c").innerHTML= "Il prezzo scontato è: " + prezzoFinale.toFixed(2) +"€"
 
 } else if (età >= 65) {
     sconto = prezzoPerChilometro * 40 / 100
+    document.getElementById("b").innerHTML= "Lo sconto è: " + sconto.toFixed(2) + "€"
+    console.log(sconto)
+    prezzoFinale = prezzoPerChilometro - sconto;
+    document.getElementById("c").innerHTML= "Il prezzo scontato è: " + prezzoFinale.toFixed(2) +"€"
 }else{
-    sconto = prezzoPerChilometro
+    sconto = 0
+    document.getElementById("b").innerHTML= "Lo sconto è: " + sconto.toFixed(2) + "€"
+    console.log(sconto)
+    prezzoFinale = prezzoPerChilometro;
+    document.getElementById("c").innerHTML= "Il prezzo è: " + prezzoPerChilometro.toFixed(2) +"€"
 }
-console.log(sconto)
-
-const prezzoFinale = prezzoPerChilometro - sconto;
 
 console.log(prezzoFinale.toFixed(2) + "€")
